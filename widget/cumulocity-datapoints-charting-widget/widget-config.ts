50c33d72127b0bf2d36b4802a17861ab76d5a039
y509144 <120079825+y509144@users.noreply.github.com>
2023-01-03 14:57:36 +0530

    Add files via upload

---
+++ widget/cumulocity-datapoints-charting-widget/widget-config.ts
@@ -0,0 +1,171 @@
+/** @format */
+
+//
+// Helper classes and interfaces
+//
+import { formatDate } from "@angular/common";
+import { ChartConfig } from "./widget-charts";
+import { has, set, get } from 'lodash';
+
+/**
+ * These elements can form the criteria
+ * for selecting measurements from c8y
+ */
+export class MeasurementOptions {
+    deviceId: string;
+    name: string;
+    fragment: string;
+    series: string;
+    pageSize: number;
+    queryDateFormat: string;
+    locale: string;
+    avgPeriod: number;
+    dateFrom?: Date;
+    dateTo?: Date;
+    targetGraphType: string;
+    timeBucket: boolean;
+    bucketPeriod: string;
+    labelDateFormat: string;
+    numdp: number;
+    sizeBuckets: number;
+    mnBuckets: number;
+    mxBuckets: number;
+    groupby: boolean;
+    cumulative: boolean;
+    group: string;
+
+    constructor(
+        averagePeriod: number,
+        targetGraphType: string,
+        numdp: number,
+        sizeBuckets: number,
+        mnBuckets: number,
+        mxBuckets: number,
+        groupby: boolean,
+        cumulative: boolean,
+        group: string = "default"
+    ) {
+        this.pageSize = 50;
+        this.queryDateFormat = "yyyy-MM-ddTHH:mm:ssZ";
+        this.locale = "en";
+        this.avgPeriod = averagePeriod;
+        this.targetGraphType = targetGraphType;
+        this.timeBucket = false;
+        this.bucketPeriod = "minute";
+        this.labelDateFormat = "HH:mm";
+        this.numdp = numdp;
+        this.sizeBuckets = sizeBuckets;
+        this.mnBuckets = mnBuckets;
+        this.mxBuckets = mxBuckets;
+        this.groupby = groupby;
+        this.cumulative = cumulative;
+        this.group = group;
+    }
+
+    public setFilter(
+        deviceId: string,
+        name: string,
+        fragment: string,
+        series: string,
+        from: Date,
+        to: Date,
+        count: number,
+        targetGraphType: string,
+        timeBucket: boolean,
+        bucketPeriod: string,
+        labelDateFormat: string
+    ) {
+        this.deviceId = deviceId;
+        this.name = name;
+        this.fragment = fragment;
+        this.series = series;
+        if (from) {
+            set(this, "dateFrom", from);
+        }
+
+        if (to) {
+            set(this, "dateTo", to);
+        }
+        this.pageSize = count;
+        this.targetGraphType = targetGraphType;
+        this.timeBucket = timeBucket;
+        this.bucketPeriod = bucketPeriod;
+        this.labelDateFormat = labelDateFormat;
+    }
+
+    public filter(): Object {
+        let filter = {};
+        set(filter, "source", this.deviceId);
+        set(filter, "valueFragmentType", this.fragment);
+        set(filter, "valueFragmentSeries", this.series);
+        set(filter, "pageSize", 2000);
+        set(filter, "revert", true);
+        set(filter, "withTotalPages", true);
+
+        //dates are strings in the filter
+        if (has(this, "dateFrom")) {
+            set(filter, "dateFrom", formatDate(get(this, "dateFrom"), this.queryDateFormat, this.locale));
+        }
+
+        //this should always be "now" for the moment it can't be entered manually
+        if (has(this, "dateTo")) {
+            set(filter, "dateTo", formatDate(get(this, "dateTo"), this.queryDateFormat, this.locale));
+        }
+
+        return filter;
+    }
+}
+
+
+
+export interface DataObject {
+    data: any;
+    key: string;
+    options: MeasurementOptions;
+}
+
+/**
+ * All multi selects can handle object lists
+ * this defines one that can be used with the
+ * select definition in this file.
+ *
+ * optional generic field for label/text/date formatting etc
+ */
+export interface RawListItem {
+    /**
+     * FIXME: changing to number | string reveals the different interactions with this property.
+     * Sometimes its a number used to access a position in an array.
+     * Sometimes its a string used to access attributes of an object.
+     * This leads to a high risk of issues!
+     */
+    id: any 
+    text: string;
+    format?: string;
+    isGroup?: boolean;
+    groupname?: string;
+}
+
+/**
+ * This class will contain all the bespoke config for the widget
+ */
+export class WidgetConfig {
+    /**
+     * Members for the config
+     */
+    selectedDevices: RawListItem[];
+    selectedMeasurements: RawListItem[];
+
+    /**
+     * charts configuration
+     */
+    chart: ChartConfig;
+    changed = false;
+
+    /**
+     *  Create an instance of the config object
+     */
+    constructor() {
+        this.selectedDevices = [];
+        this.selectedMeasurements = [];
+    }
+}
---
+++ widget/cumulocity-datapoints-charting-widget/widget-helper.ts
@@ -0,0 +1,136 @@
+/**
+ * WidgetHelper is a utility class that will provide useful function to
+ * authors when creating new widgets for Cumulocity.
+ *
+ * The main hook is to create an instance of the class in your widget
+ * and then use whatever functionality you need.
+ *
+ * Widgets have config in cumulocity which gets updated when you
+ * invoke the config page for the widget. This class wraps that
+ * and provides useful common functionally on top.
+ *
+ * @format
+ */
+
+ import { has, set, get } from "lodash";
+ import { v4 as uuidv4 } from "uuid";
+ import { ChartConfig } from "./widget-charts";
+ import { MeasurementHelper } from "./widget-measurements";
+ 
+ /**
+  * The C8Y process has a standard member "config". This member has
+  * the data from the config screen in it along with standard data
+  * that might be set. We create a member of the config option containing
+  * our set of configuration data.
+  *
+  * typically each widget class that accesses the config data (to set or retrieve)
+  * will have an instance of this class. It will be created in the constructor
+  * to default the config and ngInit to make sure it is up to date.
+  *
+  * The CONFIGTYPE generic is the class of WidgetConfig (or extended type TBD)
+  */
+ export class WidgetHelper<CONFIGTYPE> {
+     /**
+      * The following 2 members are for checking types
+      * do not store data in these!!!!
+      */
+     private reference: CONFIGTYPE;
+     private chartRef: ChartConfig;
+ 
+     /**
+      *  member that holds the actual data
+      */
+     private config: CONFIGTYPE;
+     private measurements: MeasurementHelper;
+     private rawConfig: any;
+ 
+     /**
+      *
+      * E.G. let h = new WidgetHelper(config, MyConfigType); // type argument inference
+      *
+      * @param c is the configuration member supplied by default
+      * @param ConfigCreator The type of the Custom Widget Class
+      */
+     constructor(c: Object, ConfigCreator: new () => CONFIGTYPE) {
+         this.rawConfig = c;
+         this.reference = new ConfigCreator(); //template
+         this.chartRef = new ChartConfig(); //NOT for data
+ 
+         // only set if it doesn't exist
+         if (!has(c, "customwidgetdata")) {
+             this.config = new ConfigCreator();
+         } else {
+             // because this is stored and retrieved from mongo db
+             // reset the prototype and leave the data
+             this.config = get(c, "customwidgetdata");
+             if (Object.getPrototypeOf(this.config) !== Object.getPrototypeOf(this.reference)) {
+                 Object.setPrototypeOf(this.config, Object.getPrototypeOf(this.reference));
+             }
+         }
+     }
+ 
+     getDeviceTarget(): string | undefined {
+         if (has(this.rawConfig, "device")) {
+             return this.rawConfig["device"].id;
+         } else if (has(this.rawConfig, "settings")) {
+             if (has(this.rawConfig["settings"], "context")) {
+                 return this.rawConfig["settings"]["context"].id;
+             }
+         }
+         return undefined;
+     }
+ 
+     /**
+      * Use this member when accessing the configuration data
+      *
+      * @returns a reference to the widgets configuration with class methods
+      */
+     getWidgetConfig(): CONFIGTYPE {
+         return this.config;
+     }
+ 
+     /**
+      * Set the customwidgetdata member with the current config
+      * typically call this when updating the config in a form
+      *
+      * @param c config member from the custom widget
+      */
+     setWidgetConfig(c: any) {
+         set(c, "customwidgetdata", this.config);
+     }
+ 
+     getUniqueID(): string {
+         if (!has(this.config, "uuid")) {
+             set(this.config as any, "uuid", uuidv4());
+         }
+         return get(this.config, "uuid");
+     }
+ 
+ /**
+  * If an object exists it will be returned with the correct prototype
+  * If it doesn't it will be created and a default returned.
+  *
+  * @returns Chart config object attached to the general configuration
+  */
+     getChartConfig(): ChartConfig {
+         if (has(this.config, "chart")) {
+             const chartConfig: ChartConfig = get(this.config, "chart");
+             if (Object.getPrototypeOf(chartConfig) !== Object.getPrototypeOf(this.chartRef)) {
+                 Object.setPrototypeOf(chartConfig, Object.getPrototypeOf(this.chartRef));
+             }
+             return chartConfig;
+         } else {
+             //add new ? or perhaps throw if we get more serious
+             const cfg = set(this.config as any, "chart", new ChartConfig());
+             return cfg.chart;
+         }
+     }
+ 
+     getMeasurements(): MeasurementHelper {
+         if (!this.measurements) {
+             this.measurements = new MeasurementHelper();
+         }
+         return this.measurements;
+     }
+ }
+ 
